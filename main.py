c0aa5f69f336f50ff0415debc907e46bd067c4f6
Иван Карашов <karashov@metasharks.ru>
2022-12-20 00:25:42 +0300

    refactor

--- main.py
+++ main.py
@@ -12,12 +12,9 @@ from parent_classes.base_disc_array import BaseDiscArray
 from utils import collect, request_to_array
 
 # every BaseDiscArray`s children (subclasses) here:
-# pylint: disable=locally-disabled
 from huawei import Huawei  # noqa: F401
-# pylint: disable=locally-disabled
-from hitachi import Hitachi  # noqa: F401 pylint: disable=locally-disabled
-# pylint: disable=locally-disabled
-from ibm import IBM  # noqa: F401 pylint: disable=locally-disabled
+from hitachi import Hitachi  # noqa: F401
+from ibm import IBM  # noqa: F401
 
 
 def get_disc_array_class(*, __vendor_name: str) -> Type[BaseDiscArray]:
